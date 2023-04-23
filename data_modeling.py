9057986a59afd412630bf3f6e52df304627e852b
MUpreti10 <34248785+MUpreti10@users.noreply.github.com>
2023-04-23 13:49:02 +0700

    Update data_modeling.py

--- data_modeling.py
+++ data_modeling.py
@@ -1,61 +1,204 @@
 
 class User:
-    def __init__(self, user_id: int, name: str, email: str):
-        self.user_id = user_id
-        self.name = name
+    def __init__(self, id, username, password, email, created_at):
+        self.id = id
+        self.username = username
+        self.password = password
         self.email = email
+        self.created_at = created_at
+    
+    def create(self):
+        # Code to create a new user in the database
+    
+    def update(self):
+        # Code to update an existing user in the database
+    
+    def delete(self):
+        # Code to delete a user from the database
+
 
 class Storefront:
-    def __init__(self, storefront_id: int, user_id: int, name: str):
-        self.storefront_id = storefront_id
+    def __init__(self, id, user_id, name, is_microstore, created_at):
+        self.id = id
         self.user_id = user_id
         self.name = name
+        self.is_microstore = is_microstore
+        self.created_at = created_at
+    
+    def create(self):
+        # Code to create a new storefront in the database
+    
+    def update(self):
+        # Code to update an existing storefront in the database
+    
+    def delete(self):
+        # Code to delete a storefront from the database
+    
+    def get_promotional_products(self):
+        # Code to get promotional products for this storefront
+    
+    def get_catalogs(self):
+        # Code to get catalogs for this storefront
 
 class MicroStore:
-    def __init__(self, microstore_id: int, user_id: int, name: str):
-        self.microstore_id = microstore_id
+    def __init__(self, id, user_id, name, is_microstore, created_at):
+        self.id = id
         self.user_id = user_id
         self.name = name
+        self.is_microstore = is_microstore
+        self.created_at = created_at
+    
+    def create(self):
+        # Code to create a new microstore in the database
+    
+    def update(self):
+        # Code to update an existing microstore in the database
+    
+    def delete(self):
+        # Code to delete a microstore from the database
+    
+    def get_products(self):
+        # Code to get all products for this microstore
 
-class Catalog:
-    def __init__(self, catalog_id: int, storefront_id: int, name: str):
-        self.catalog_id = catalog_id
-        self.storefront_id = storefront_id
-        self.name = name
-
+        
 class Product:
-    def __init__(self, product_id: int, name: str, description: str, price: float, available_date: datetime.date, stock_quantity: int):
-        self.product_id = product_id
+    def __init__(self, id: int, name: str, description: str, price: float, 
+                 available_date: datetime, stock_quantity: int, image_url: str):
+        self.id = id
         self.name = name
         self.description = description
         self.price = price
         self.available_date = available_date
         self.stock_quantity = stock_quantity
-
-class Attribute:
-    def __init__(self, attribute_id: int, product_id: int, name: str, value: str):
-        self.attribute_id = attribute_id
+        self.image_url = image_url
+        
+    def create(self):
+        # code to create a new product
+    
+    def update(self):
+        # code to update an existing product
+    
+    def delete(self):
+        # code to delete a product
+    
+    def get_attributes(self) -> List[ProductAttribute]:
+        # code to get all attributes for this product
+        pass
+        
+class ProductAttribute:
+    def __init__(self, id: int, product_id: int, title: str, description: str):
+        self.id = id
         self.product_id = product_id
+        self.title = title
+        self.description = description
+        
+    def create(self):
+        # code to create a new product attribute
+    
+    def update(self):
+        # code to update an existing product attribute
+    
+    def delete(self):
+        # code to delete a product attribute
+        
+class Catalog:
+    def __init__(self, name: str, storefront_id: int):
         self.name = name
-        self.value = value
+        self.storefront_id = storefront_id
+        self.id = None
 
-class Image:
-    def __init__(self, image_id: int, product_id: int, url: str):
-        self.image_id = image_id
+    def create(self) -> int:
+        # Code to insert new catalog into the database
+        self.id = 123  # Set the ID of the new catalog
+        return self.id
+
+    def update(self) -> bool:
+        # Code to update the catalog in the database
+        return True
+
+    def delete(self) -> bool:
+        # Code to delete the catalog from the database
+        return True
+
+    def get_products(self) -> List[int]:
+        # Code to get a list of product IDs in this catalog from the database
+        return [1, 2, 3]  # Dummy data for now
+
+
+class CatalogProduct:
+    def __init__(self, catalog_id: int, product_id: int):
+        self.catalog_id = catalog_id
         self.product_id = product_id
-        self.url = url
+        self.id = None
 
-class Category:
-    def __init__(self, category_id: int, name: str):
-        self.category_id = category_id
+    def create(self) -> int:
+        # Code to insert new catalog product relationship into the database
+        self.id = 456  # Set the ID of the new catalog product relationship
+        return self.id
+
+    def update(self) -> bool:
+        # Code to update the catalog product relationship in the database
+        return True
+
+    def delete(self) -> bool:
+        # Code to delete the catalog product relationship from the database
+        return True
+
+
+class StoreProduct:
+    def __init__(self, storefront_id: int, product_id: int):
+        self.storefront_id = storefront_id
+        self.product_id = product_id
+        self.id = None
+
+    def create(self) -> int:
+        # Code to insert new store product relationship into the database
+        self.id = 789  # Set the ID of the new store product relationship
+        return self.id
+
+    def update(self) -> bool:
+        # Code to update the store product relationship in the database
+        return True
+
+    def delete(self) -> bool:
+        # Code to delete the store product relationship from the database
+        return True
+
+
+class ProductCategory:
+    def __init__(self, name: str):
         self.name = name
+        self.id = None
 
-class SearchHistory:
-    def __init__(self, search_id: int, user_id: int, keyword: str, category_id: int, min_price: float, max_price: float, in_stock: bool):
-        self.search_id = search_id
-        self.user_id = user_id
-        self.keyword = keyword
+    def create(self) -> int:
+        # Code to insert new product category into the database
+        self.id = 234  # Set the ID of the new product category
+        return self.id
+
+    def update(self) -> bool:
+        # Code to update the product category in the database
+        return True
+
+    def delete(self) -> bool:
+        # Code to delete the product category from the database
+        return True
+
+
+class ProductCategoryMap:
+    def __init__(self, product_id: int, category_id: int):
+        self.product_id = product_id
         self.category_id = category_id
-        self.min_price = min_price
-        self.max_price = max_price
-        self.in_stock = in_stock
+        self.id = None
+
+    def create(self) -> int:
+        # Code to insert new product category mapping into the database
+        self.id = 567  # Set the ID of the new product category mapping
+        return self.id
+
+    def update(self) -> bool:
+        # Code to update the product category mapping in the database
+        return True
+
+    def delete(self) -> bool:
+        # Code to delete the product category mapping from the database
+        return True
